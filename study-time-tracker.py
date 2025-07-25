04b2cb0a971fbd820a66bde5301bfce1e71ddd43
Aya301-maker <ayaboukssim2006ab@icloud.com>
2025-07-25 16:29:45 +0100

    Add files via upload

---
+++ study-time-tracker.py
@@ -0,0 +1,6 @@
+# tracker.py
+subject = input("What subject did you study? ")
+hours = input("How many hours? ")
+date = input("Enter the date (YYYY-MM-DD): ")
+
+print(f"You studied {subject} for {hours} hours on {date}.")
